13e2078e4edf187ff30ad6ddc7febd07ee1c6508
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-04-21 04:31:00 +0500

    Building Homse Secton Silde Line

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,31 +1,45 @@
 type newProp = {
-    news:any
-}
+  news: any;
+};
 
-function Home(props:newProp) {
-    console.log(props.news[0])
+function Home(props: newProp) {
   return (
-    <div className=" flex items-center bg-gray-400 pl-28 h-screen w-screen">
-        <div className="grid grid-cols-2 p-5 rounded-md bg-white">
-        <div className=" w-72">
-            <img src={props?.news[1]?.urlToImage} className=" w-72 h-44 rounded-md" />
-            <h1 className=" font-bold text-sm">{props?.news[1]?.source?.name.toUpperCase()}</h1>
-            <h1 className="text-xl w-72 hover:underline">{props?.news[1]?.title}</h1>
-        </div>
+    <div className=" flex items-center bg-gray-400 pl-28 mt-5 w-screen ">
+      <div className="grid grid-cols-2 p-5 rounded-md bg-white">
+        <a href={props?.news[1]?.url}>
+          <div className=" w-72">
+            <h1 className=" text-blue-600 text-xl">Top Stories</h1>
+            <br className=" mt-5" />
+            <img
+              src={props?.news[1]?.urlToImage}
+              className=" w-72 h-44 rounded-md"
+            />
+            <h1 className=" font-bold text-sm">
+              {props?.news[1]?.source?.name.toUpperCase()}
+            </h1>
+            <h1 className="text-xl w-72 hover:underline">
+              {props?.news[1]?.title}
+            </h1>
+          </div>
+        </a>
         <div className=" w-60 ml-7">
-            <h1 className=" font-semibold text-xs mt-3">{props?.news[0]?.source?.name.toUpperCase()}</h1>
-            <h1 className="text-sm  hover:underline">{props?.news[0]?.title}</h1>
-            <h1 className=" font-semibold text-xs mt-3">{props?.news[2]?.source?.name.toUpperCase()}</h1>
-            <h1 className="text-sm  hover:underline">{props?.news[2]?.title}</h1>
-            <h1 className=" font-semibold text-xs mt-3">{props?.news[3]?.source?.name.toUpperCase()}</h1>
-            <h1 className="text-sm  hover:underline">{props?.news[3]?.title}</h1>
+          <h1 className=" font-semibold text-xs mt-3">
+            {props?.news[0]?.source?.name.toUpperCase()}
+          </h1>
+          <a href={props?.news[0]?.url}> <h1 className="text-sm  hover:underline">{props?.news[0]?.title}</h1></a>
+          <h1 className=" font-semibold text-xs mt-3">
+            {props?.news[2]?.source?.name.toUpperCase()}
+          </h1>
+          <a href={props?.news[2]?.url}><h1 className="text-sm  hover:underline">{props?.news[2]?.title}</h1></a>
+          <h1 className=" font-semibold text-xs mt-3">
+            {props?.news[3]?.source?.name.toUpperCase()}
+          </h1>
+          <a href={props?.news[3]?.url}><h1 className="text-sm  hover:underline">{props?.news[3]?.title}</h1></a>
         </div>
+      </div>
+      <div></div>
     </div>
-    <div>
-    
-    </div>
-    </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Menubar from "./Menubar";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import moment from "moment";
 
 function Main() {
 
@@ -12,7 +13,7 @@ function Main() {
 
     var url = 'https://newsapi.org/v2/everything?' +
           'q=Apple&' +
-          'from=2024-04-18&' +
+          'from=2024-04-19&' +
           'sortBy=popularity&' +
           'apiKey=35839542f8864448a68e188adb9a8467';
 
@@ -27,15 +28,27 @@ var req = new Request(url);
      }
   };
 
+  console.log(news[1]?.url);
+
 
   useEffect(() => {
     getNews();
   }, []);
+
+  const date:any = new Date()
+
+
   return (
     <>
+   
       <Navbar />
       <Menubar />
+      <div className=" bg-gray-100 pt-5">
+        <h1 className=" ml-28 text-3xl">Your briefing</h1>
+        <h1 className=" ml-28 text-gray-500 text-sm mt-2">{moment(date).format("DD-MM-YY")}</h1>
       <Home news={news}/>
+      </div>
+      
     </>
   );
 }
